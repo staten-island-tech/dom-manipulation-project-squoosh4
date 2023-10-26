02e1fd1d39c5bf1f0b1a22c28847bad8dbc85b41
squoosh4 <110200988+squoosh4@users.noreply.github.com>
2023-10-26 13:12:36 -0400

    awae
    afafw

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const DOMSelector = {
     imageForm: document.getElementById("img"),
     submitButton: document.getElementById("submit-button"),
     text: document.querySelectorAll(".op"),
+    gallery: document.querySelector(".gallery")
 
 };
 
@@ -13,16 +14,44 @@ console.log(DOMSelector.text);
 const testTextArray = Array.from(DOMSelector.text);
 console.log(testTextArray);
 
-const gameCard = {
+//const gameCard = {
+//
+//    gameName: DOMSelector.h2Form,
+//    gameImage: DOMSelector.imageForm
+//
+//};
 
-    gameName: DOMSelector.h2Form,
-    gameImage: DOMSelector.imageForm
+function makeCard() {
+
+    const card = {
+
+        gameName: DOMSelector.h2Form,
+        imageAddress: DOMSelector.imageForm
+
+    }
 
 };
 
-function makeCard(card_forGame) {
+function clearFields(fieldValue) {
 
-
+    fieldValue.value = ""
 
 };
+
+DOMSelector.submitButton.addEventListener ("click", function(){
+
+    event.preventDefault();
+    DOMSelector.gallery.insertAdjacentHTML(
+        "beforeend",
+        `
+        <div class="card">
+            <img src="${DOMSelector.imageForm}" class="image">
+            <h2 class="h2">${DOMSelector.h2Form}</h2>
+        /div>
+        `
+    );
+    clearFields(DOMSelector.h2Form);
+    clearFields(DOMSelector.imageForm);
+
+});
 
