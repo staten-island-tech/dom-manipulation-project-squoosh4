69a1e7ad67d8ed162ddb9f7befcb57cef82a54f2
squoosh4 <110200988+squoosh4@users.noreply.github.com>
2023-11-10 12:53:58 -0500

    m b
    cngf

--- app.js
+++ app.js
@@ -1,18 +1,7 @@
-const DOMSelector = {
+//console.log(DOMSelector.text);
 
-    form: document.getElementById("form"),
-    h2Form: document.getElementById("h2"),
-    imageForm: document.getElementById("img"),
-    submitButton: document.getElementById("submit-button"),
-    text: document.querySelectorAll(".op"),
-    gallery: document.querySelector(".gallery")
-
-};
-
-console.log(DOMSelector.text);
-
-const testTextArray = Array.from(DOMSelector.text);
-console.log(testTextArray);
+//const testTextArray = Array.from(DOMSelector.text);
+//console.log(testTextArray);
 
 //const gameCard = {
 //
@@ -32,26 +21,62 @@ console.log(testTextArray);
 //
 //};
 
+const DOMSelector = {
+
+    form: document.getElementById("form"),
+    h2Form: document.getElementById("h2"),
+    imageForm: document.getElementById("img"),
+    submitButton: document.getElementById("submit-button"),
+    text: document.querySelectorAll(".op"),
+    gallery: document.querySelector(".gallery")
+
+};
+
+const cardObj = {
+
+    name: DOMSelector.h2Form.value,
+    image: DOMSelector.imageForm.value
+
+};
+
 function clearFields(fieldValue) {
 
     fieldValue.value = ""
 
 };
 
-DOMSelector.submitButton.addEventListener ("click", function(){
+function remove() {
+    
+    let buttons = document.querySelectorAll("button");
+    buttons.forEach((btn) =>
+        btn.addEventListener("click", function(event) {
+            event.target.parentElement.remove();
+        })
+    );
+};
 
-    event.preventDefault();
+function create() {
+
     DOMSelector.gallery.insertAdjacentHTML(
         "beforeend",
         `
         <div class="card">
-            <img src="${DOMSelector.imageForm.value}" class="image">
-            <h2 class="h2">${DOMSelector.h2Form.value}</h2>
+            <img src="${cardObj.image}" class="image">
+            <h2 class="h2">${cardObj.name}</h2>
+            <button class="deletebutton">delete this entry</button>
         </div>
         `
     );
+
+};
+
+
+DOMSelector.submitButton.addEventListener ("click", function(event){
+
+    event.preventDefault();
+    create();
     clearFields(DOMSelector.h2Form);
     clearFields(DOMSelector.imageForm);
+    remove();
 
 });
-
